ec687cbed4484b26ed359c1db9714988d5cbbd69
Sam Brandon <sbrandon0432@yahoo.com>
2021-01-15 21:20:45 -0800

    new cmop commit to tranfer data

---
+++ dataGenerator/index.js
@@ -0,0 +1,7 @@
+const faker = require('faker');
+
+const randomName = faker.name.findName(); // Rowan Nikolaus
+const randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+const randomCard = faker.helpers.createCard();
+
+
