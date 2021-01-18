6778ec6456aee1ff0a4635dd6fca3964e66eb4b0
Sam Brandon <sambrandon@Sams-MBP.hsd1.ca.comcast.net>
2021-01-17 18:18:19 -0800

    implomented working fuctions to add to the db for the seed file

--- dataGenerator/index.js
+++ dataGenerator/index.js
@@ -1,7 +1,12 @@
 const faker = require('faker');
+const { addToLEGO } = require('../database/index.js');
 
-const randomName = faker.name.findName(); // Rowan Nikolaus
-const randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
-const randomCard = faker.helpers.createCard();
+for (let i = 0; i < 1; i += 1) {
+  const productName = faker.commerce.productName();
+  const productImage = faker.image.cats();
 
+  addToLEGO(productName, productImage);
+}
 
+// we need to generate the fake data, and but in in a obj
+// we then pass in the fake data into the db
--- database/index.js
+++ database/index.js
@@ -1,16 +1,34 @@
+/* eslint-disable quotes */
 /* eslint-disable no-console */
 
-const { connection } = require('./dbKey.js');
+const { connection } = require('./config.js');
 
 connection.connect((err) => {
   if (err) {
     console.log(err);
   } else {
-    console.log('the conected to the db');
+    console.log('the connected to the db');
   }
 });
 
-connection.query('SELECT 1 + 1 AS solution', (error, results) => {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-});
+const addToLEGO = (productName, productImage) => {
+  console.log(productName);
+  const qryString = `INSERT INTO products (product_name) VALUES ("${productName}")`;
+  const qryImg = `INSERT INTO images (product_imgae) VALUES("${productImage}")`;
+
+  connection.query(qryString, (err) => {
+    if (!err) {
+      connection.query(qryImg, (error) => {
+        if (error) {
+          throw err;
+        }
+      });
+    } else {
+      throw err;
+    }
+  });
+};
+
+module.exports = {
+  addToLEGO,
+};
