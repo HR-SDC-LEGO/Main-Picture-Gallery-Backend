2e986dec703d62c3776f8c3d46ced433d83ee334
sbrandon0432 <sbrandon0432@yahoo.com>
2021-01-22 11:42:00 -0800

    fuction for zoom in place and handing down, no implomented

--- client/src/components/Carousel/MainImage.jsx
+++ client/src/components/Carousel/MainImage.jsx
@@ -5,9 +5,15 @@ import React from 'react';
 import './MainImageS.scss';
 
 const MainImage = ({ main }) => {
+
+
+  const alert = () => {
+    console.log(('clicked'));
+  }
+
   return (
     <div className="MainImage">
-      <div className="MainImageZoom" id="MainImageZoom">
+      <div className="MainImageZoom" id="MainImageZoom" onClick={alert}>
         <img src={main} alt="product" />
       </div>
     </div>
