49aba14a84ab3c68a945498c69aec572c0c9a79a
sbrandon0432 <sbrandon0432@yahoo.com>
2021-01-22 11:43:39 -0800

    code clean up

--- client/src/components/Carousel/MainImage.jsx
+++ client/src/components/Carousel/MainImage.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable no-console */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/prop-types */
 /* eslint-disable arrow-body-style */
 
@@ -5,11 +8,9 @@ import React from 'react';
 import './MainImageS.scss';
 
 const MainImage = ({ main }) => {
-
-
   const alert = () => {
     console.log(('clicked'));
-  }
+  };
 
   return (
     <div className="MainImage">
