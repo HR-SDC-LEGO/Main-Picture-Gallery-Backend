f0df3138c057f717a2f1c71f520b7bee14dc74d6
sbrandon0432 <sbrandon0432@yahoo.com>
2021-01-23 14:27:45 -0800

    re re refactor, to keep buttions i n the main image gall

--- client/src/components/Carousel/ImageList.jsx
+++ client/src/components/Carousel/ImageList.jsx
@@ -12,78 +12,84 @@ class ImageList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      // startingIndexForImageCarosel: 0,
+      startingIndexForImageCarosel: 0,
     };
-    // this.carouselUp = this.carouselUp.bind(this);
-    // this.carouselDown = this.carouselDown.bind(this);
-    // this.changeMainPicture = this.changeMainPicture.bind(this);
+    this.carouselUp = this.carouselUp.bind(this);
+    this.carouselDown = this.carouselDown.bind(this);
+    this.changeMainPicture = this.changeMainPicture.bind(this);
   }
 
-  // carouselDown() {
-  //   const { startingIndexForImageCarosel } = this.state;
-  //   const { images } = this.props;
-  //   if (images.length - 7 === startingIndexForImageCarosel) {
-  //     return;
-  //   }
-  //   this.setState({
-  //     startingIndexForImageCarosel: startingIndexForImageCarosel + 1,
-  //   });
-  // }
+  carouselDown() {
+    const { startingIndexForImageCarosel } = this.state;
+    const { images } = this.props;
+    if (images.length - 7 === startingIndexForImageCarosel) {
+      return;
+    }
+    this.setState({
+      startingIndexForImageCarosel: startingIndexForImageCarosel + 1,
+    });
+  }
 
-  // carouselUp() {
-  //   const { startingIndexForImageCarosel } = this.state;
-  //   if (startingIndexForImageCarosel === 0) {
-  //     return;
-  //   }
-  //   this.setState({
-  //     startingIndexForImageCarosel: startingIndexForImageCarosel - 1,
-  //   });
-  // }
+  carouselUp() {
+    const { startingIndexForImageCarosel } = this.state;
+    if (startingIndexForImageCarosel === 0) {
+      return;
+    }
+    this.setState({
+      startingIndexForImageCarosel: startingIndexForImageCarosel - 1,
+    });
+  }
 
-  // changeMainPicture(index) {
-  //   const { startingIndexForImageCarosel } = this.state;
-  //   const { setSelectedImageIndex } = this.props;
-  //   let targetIndex = Number(index);
-  //   let difference = 0;
-  //   const middleIndex = startingIndexForImageCarosel + 3;
-  //   setSelectedImageIndex(index);
-  //   if (targetIndex === middleIndex) {
-  //     return;
-  //   }
-  //   if (targetIndex > middleIndex) {
-  //     while (targetIndex !== middleIndex) {
-  //       targetIndex -= 1;
-  //       difference += 1;
-  //     }
-  //   } else {
-  //     while (targetIndex !== middleIndex) {
-  //       targetIndex += 1;
-  //       difference -= 1;
-  //     }
-  //   }
-  //   this.setState({
-  //     startingIndexForImageCarosel: startingIndexForImageCarosel + difference,
-  //   });
-  // }
+  changeMainPicture(index) {
+    const { startingIndexForImageCarosel } = this.state;
+    const { setSelectedImageIndex } = this.props;
+    let targetIndex = Number(index);
+    let difference = 0;
+    const middleIndex = startingIndexForImageCarosel + 3;
+    setSelectedImageIndex(index);
+    if (targetIndex === middleIndex) {
+      return;
+    }
+    if (targetIndex > middleIndex) {
+      while (targetIndex !== middleIndex) {
+        targetIndex -= 1;
+        difference += 1;
+      }
+    } else {
+      while (targetIndex !== middleIndex) {
+        targetIndex += 1;
+        difference -= 1;
+      }
+    }
+    this.setState({
+      startingIndexForImageCarosel: startingIndexForImageCarosel + difference,
+    });
+  }
 
   render() {
-    const { images, selectedImageIndex, startingIndexForImageCarosel, changeMainPicture, carouselUp, carouselDown } = this.props;
-    // const { startingIndexForImageCarosel } = this.state;
+    const { images, selectedImageIndex } = this.props;
+    const { startingIndexForImageCarosel } = this.state;
     const rangeOfRenderingCarosel = images.slice(startingIndexForImageCarosel, startingIndexForImageCarosel + 7);
-    console.log(this.props);
     return (
       <div className="ImageList">
-        <button type="button" onClick={carouselUp}>up</button>
+        <div className="left">
+          <button type="button" onClick={this.carouselDown}>TEST</button>
+        </div>
+        <button type="button" onClick={this.carouselUp}>up</button>
         {
       rangeOfRenderingCarosel.map((item, index) => {
         const imageIndex = index + startingIndexForImageCarosel;
         return (
-          <ImageListItem imgContainer={item} key={imageIndex} handleChange={changeMainPicture} id={imageIndex} isSelected={selectedImageIndex === imageIndex} />
+          <ImageListItem imgContainer={item} key={imageIndex} handleChange={this.changeMainPicture} id={imageIndex} isSelected={selectedImageIndex === imageIndex} />
         );
       })
         }
-        <button type="button" onClick={carouselDown}>down</button>
+        <button type="button" onClick={this.carouselDown}>down</button>
+        <div className="right">
+          <button type="button" onClick={this.carouselUp}>TEST</button>
+        </div>
       </div>
+
     );
   }
 }
