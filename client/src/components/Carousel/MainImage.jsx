8b8f221d9e679f70ad82f281b8294955e4738319
sbrandon0432 <sbrandon0432@yahoo.com>
2021-01-23 14:11:59 -0800

    refactor for img methods

--- client/src/components/Carousel/MainImage.jsx
+++ client/src/components/Carousel/MainImage.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable max-len */
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-cycle */
 /* eslint-disable no-console */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
@@ -6,17 +9,19 @@
 
 import React from 'react';
 import './MainImageS.scss';
+import ImageList from './ImageList';
 
-const MainImage = ({ main }) => {
+const MainImage = ({ main, changeMainPicture, carouselDown, carouselUp }) => {
   const alert = () => {
     console.log(('clicked'));
   };
-
   return (
     <div className="MainImage">
+      <button type="button" onClick={carouselDown}>down</button>
       <div className="MainImageZoom" id="MainImageZoom" onClick={alert}>
         <img src={main} alt="product" />
       </div>
+      <button type='button' onClick={carouselUp}>up</button>
     </div>
   );
 };
